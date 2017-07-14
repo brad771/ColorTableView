c7ca471fb70ebe1899159c25a766bb897bcb32db
bradly puricelli <bradpuricelli@gmail.com>
2017-07-13 17:52:05 -0700

    Finished

---
+++ ColorTableView/ColorTableView/ColorsTableViewCell.swift
@@ -0,0 +1,20 @@
+
+
+import UIKit
+
+class ColorsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ColorTableView/ColorTableView/ViewController.swift
@@ -0,0 +1,48 @@
+
+
+import UIKit
+
+class ColorsTableViewController: UITableViewController {
+    
+    var colorNames = [String]()
+    var colorColors = [UIColor] ()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colorNames = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+        colorColors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple, UIColor.brown]
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colorNames.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorsTableCell", for: indexPath) as! ColorsTableViewCell
+        
+        
+        let row = indexPath.row
+        cell.colorLabel.text = colorNames[row]
+        cell.backgroundColor = colorColors[row]
+        
+        return cell
+    }
+    
+    
+    
+}
